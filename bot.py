229014c7d22b1883d4a892df31760d3ca2cecf89
Matt D <mdann52@yahoo.co.uk>
2025-02-23 10:04:05 +0000

    Add instructions replacements

--- bot.py
+++ bot.py
@@ -7,15 +7,17 @@ import mwparserfromhell
 import ipaddress
 import pywikibot.logentries
 import pywikibot.pagegenerators
+import sys
 import traceback
 
 directory = os.path.dirname(os.path.realpath(__file__))
 
-logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
                     filename=os.path.join(directory, 'aivBot.log'),
                     level=logging.INFO)
-logging.getLogger().addHandler(logging.StreamHandler())
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
 
 SETTING_REGEX = r"<!-- v[0-9]\.[0-9]\.[0-9]{1,3} RemoveBlocked=([ofnOFN]{2,3}) MergeDuplicates=([ofnOFN]{2,3}) AutoMark=([ofnOFN]{2,3}) FixInstructions=([ofnOFN]{2,3}) AutoBacklog=([ofnOFN]{2,3}) AddLimit=([0-9]{1,2}) RemoveLimit=([0-9]{1,2}) -->"
 pages_to_watch = [
@@ -40,6 +42,19 @@ special_ips = {}
 special_cats = []
 instructions = []
 
+class Settings:
+    def __init__(self, raw_data):
+        settings = re.search(SETTING_REGEX, raw_data)
+        self.RemovedBlocked = (settings.group(1).upper() == "ON")
+        self.MergeDuplicates = (settings.group(2).upper() == "ON")
+        self.AutoMark = (settings.group(3).upper() == "ON")
+        self.FixInstructions = (settings.group(4).upper() == "ON")
+        self.AutoBacklog =  (settings.group(5).upper() == "ON")
+        self.AddLimit = int(settings.group(6))
+        self.RemoveLimit = int(settings.group(7))
+    def __str__(self):
+        return "{},{},{},{},{},{},{}".format(self.RemovedBlocked, self.MergeDuplicates, self.AutoMark, self.FixInstructions, self.AutoBacklog, self.AddLimit, self.RemoveLimit)
+    
 def get_ip_list():
     logger.debug("Fetching special IP list")
 
@@ -53,16 +68,60 @@ def get_ip_list():
         elif line[0:14] == "* [[:Category:":
             special_cats.append(line[5:-2])
 
+def check_instructions(page: pywikibot.Page):
+    instructionPage = pywikibot.Page(enwiki, page.title()+"/instructions")
+    if instructionPage.exists():
+        # Compare instruction page content to live page content
+        rex = re.compile("<!-- HBC AIV helperbot BEGIN INSTRUCTIONS -->\n(.*)\n<!-- HBC AIV helperbot END INSTRUCTIONS -->", flags=re.DOTALL)
+        match = rex.search(instructionPage.text)
+        sections = mwparserfromhell.parse(page.text).get_sections(matches="User-reported")
+        instruction_regex = re.compile('=== User-reported ===\n'+ re.escape(match.group(1)) + '\n\n\\*', flags=re.DOTALL)
+        if instruction_regex.match(str(sections[0])) is None:
+            print('=== User-reported ===\n' + match.group(1) + "\n\\*")
+            logger.info("Replacing instructions")
+            page.text = re.sub('=== User-reported ===\n<!--.*-->', '=== User-reported ===\n', page.text, flags=re.DOTALL)
+            page.text = re.sub('=== User-reported ===.*?\\*', '=== User-reported ===\n' + match.group(1) + "\n\n*", page.text, flags=re.DOTALL)
+            page.save(minor=True, summary="Reinstating page instructions")
+            return True
+    return False
 
-def doSave(page, minor, pagetitle, summary):
+def do_save(page: pywikibot.Page, minor: bool, pagetitle: str, summary: str):
     check = pywikibot.Page(enwiki, title=pagetitle).latest_revision_id
-    if page.latest_revision_id = check:
+    if page.latest_revision_id == check:
         page.save(minor=minor, summary=summary)
     else:
         logger.warning("Revision ID changed, aborting edit: %s != %s", page.latest_revision_id, check)
         raise pywikibot.EditConflict
 
+def CheckBacklogged(page, settings):
+    content = mwparserfromhell.parse(page.text)
+    all_temps = content.filter_templates()
+    vandals = [t for t in all_temps if t.name.lower() in ["vandal", "ipvandal", "user-uaa"]]
+    vandalCount = len(vandals)
+
+    for t in all_temps:
+        if t.name == "noadminbacklog":
+            if vandalCount >= settings.AddLimit:
+                logging.info("Marking %s as backlogged", page.title())
+                newt = mwparserfromhell.nodes.Template(name="adminbacklog")
+                newt.add("bot", "HBC AIV helperbot14")
+                content.replace(t, newt)
+                page.text = content
+                do_save(summary=str(vandalCount)+" reports remaining. Noticeboard is backlogged")
+                return True
+        if t.name == "adminbacklog":
+            if vandalCount <= settings.RemoveLimit:
+                logging.info("Marking %s as unbacklogged", page.title())
+                newt = mwparserfromhell.nodes.Template(name="noadminbacklog")
+                newt.add("bot", "HBC AIV helperbot14")
+                content.replace(t, newt)
+                page.text = content
+                do_save(summary=str(vandalCount)+" reports remaining. Noticeboard is no longer backlogged")
+                return True
+    return False
+
 while True:
+    logger.info("Startup complete")
     get_ip_list()
     for pagetitle in pages_to_watch:
         try:
@@ -76,17 +135,15 @@ while True:
             vandalCount = len(vandals)
 
             # First, get page settings
-            settings = re.search(SETTING_REGEX, page.text)
-            RemovedBlocked = (settings.group(1).upper() == "ON")
-            MergeDuplicates = (settings.group(2).upper() == "ON")
-            AutoMark = (settings.group(3).upper() == "ON")
-            FixInstructions = (settings.group(4).upper() == "ON")
-            AutoBacklog =  (settings.group(5).upper() == "ON")
-            AddLimit = int(settings.group(6))
-            RemoveLimit = int(settings.group(7))
+            settings = Settings(page.text)
 
-            logger.info("Updated Settings for %s: %s,%s,%s,%s,%s,%s,%s", page.title(), RemovedBlocked, MergeDuplicates, AutoMark, FixInstructions, AutoBacklog, AddLimit,
-                        RemoveLimit)
+            logger.info("Updated Settings for %s: %s", page.title(), str(settings))
+
+            # Check instructions are up to date, if present
+            if check_instructions(page):
+                break
+
+            # next, check if any users need removing
             lines = page.text.split("\n")
 
             for idx, f in enumerate(lines):
@@ -101,13 +158,13 @@ while True:
                     partialBlock = True
 
                     # check tags
-                    if "<!--marked-->" not in f and AutoMark:
+                    if "<!--marked-->" not in f and settings.AutoMark:
                         if userInfo.isAnonymous():
                             if username in special_ips:
                                 content.replace(f, f+"<!--marked-->\n:*'''Note:''' "+special_ips[username] + ". ~~~~")
                                 page.text = content
                                 logger.info("Marking %s as a sensitive IP", username)
-                                doSave(page, False, pagetitle, str(vandalCount)+" reports remaining. Commenting on " + username + " : Sensitive IP")
+                                do_save(page, False, pagetitle, str(vandalCount)+" reports remaining. Commenting on " + username + " : Sensitive IP")
                                 break
                         # Get user categories
                         for cat in userInfo.categories():
@@ -115,7 +172,7 @@ while True:
                                 content.replace(f, f+"<!--marked-->\n:*'''Note:''' User is in the category: "+ cat + ". ~~~~")
                                 page.text = content
                                 logger.info("Marking %s as belonging to an important category", username)
-                                doSave(page, False, pagetitle, str(vandalCount)+" reports remaining. Commenting on " + username + " : User is in the category " + cat)
+                                do_save(page, False, pagetitle, str(vandalCount)+" reports remaining. Commenting on " + username + " : User is in the category " + cat)
                                 break
                     isLocked = False
                     if not userInfo.isAnonymous():
@@ -142,7 +199,7 @@ while True:
                                     partialBlock = False
 
                         counter = idx
-                        while RemovedBlocked and counter < len(lines):
+                        while settings.RemovedBlocked and counter < len(lines):
                             if lines[counter] == "":
                                 counter += 1
 
@@ -199,7 +256,7 @@ while True:
                             summary += " ([[User:HBC AIV helperbot/Legend|" + " ".join(flags) + "]])"
                         logger.info('Saving with summary "%s"', summary)
                         page.text = content
-                        doSave(page, False, pagetitle, summary)
+                        do_save(page, False, pagetitle, summary)
                         time.sleep(5)
                         break
 
@@ -208,24 +265,24 @@ while True:
             all_temps = mwparserfromhell.parse(content).filter_templates()
             vandals = [t for t in all_temps if t.name.lower() in ["vandal", "ipvandal", "user-uaa"]]
             vandalCount = len(vandals)
-            if AutoBacklog:
+            if settings.AutoBacklog:
                 for t in all_temps:
                     if t.name == "noadminbacklog":
-                        if vandalCount >= AddLimit:
+                        if vandalCount >= settings.AddLimit:
                             logger.info("Marking %s as backlogged", page.title())
                             newt = mwparserfromhell.nodes.Template(name="adminbacklog")
                             newt.add("bot", "HBC AIV helperbot14")
                             content.replace(t, newt)
                             page.text = content
-                            doSave(page, False, pagetitle, str(vandalCount)+" reports remaining. Noticeboard is backlogged.")
+                            do_save(page, False, pagetitle, str(vandalCount)+" reports remaining. Noticeboard is backlogged.")
                     if t.name == "adminbacklog":
-                        if vandalCount <= RemoveLimit:
+                        if vandalCount <= settings.RemoveLimit:
                             logger.info("Marking %s as unbacklogged", page.title())
                             newt = mwparserfromhell.nodes.Template(name="noadminbacklog")
                             newt.add("bot", "HBC AIV helperbot14")
                             content.replace(t, newt)
                             page.text = content
-                            doSave(page, False, pagetitle, str(vandalCount)+" reports remaining. Noticeboard is no longer backlogged")
+                            do_save(page, False, pagetitle, str(vandalCount)+" reports remaining. Noticeboard is no longer backlogged")
         except Exception as e:
             logger.exception(e)
             continue 
